7c302b17fe337ff7a766e816479a01cb18acae14
Madalin <madalin_bleoca@yahoo.com>
2024-05-19 19:19:09 +0300

    Creating the Outline for Student Class

---
+++ src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse;
+}
--- src/main/java/StudentDatabase.java
+++ src/main/java/StudentDatabase.java
@@ -1,6 +1,6 @@
 public class StudentDatabase {
     public static void main(String[] args) {
-
+        System.out.println("Welcome again!");
 
     }
 }
