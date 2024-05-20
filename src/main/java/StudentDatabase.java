be07600e48833bc27135c6f0259c8babd73bcb9e
Madalin <madalin_bleoca@yahoo.com>
2024-05-20 20:47:28 +0300

    Build constructor and generate a Unique ID for every student in Student.java Class

--- src/main/java/StudentDatabase.java
+++ src/main/java/StudentDatabase.java
@@ -1,6 +1,9 @@
+
 public class StudentDatabase {
     public static void main(String[] args) {
         System.out.println("Welcome again!");
+
+        Student student_1 = new Student();
 
     }
 }
